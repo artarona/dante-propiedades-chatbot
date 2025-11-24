d87e1f60fc22be5ec075713f77f3d04558342de7
artarona <artaronacorreo@gmail.com>
2025-11-24 14:13:51 -0300

    Add files via upload

---
+++ frontend/assets/js/chatbot.js
@@ -0,0 +1,11 @@
+// JavaScript básico para funcionalidad del chatbot
+console.log('Dante Chatbot cargado');
+
+// Funciones básicas de utilidad
+function showLoading() {
+    console.log('Cargando...');
+}
+
+function hideLoading() {
+    console.log('Carga completada');
+}
